b45b8aee840b7f0ccb824d1a50cfd59e9fefb1ee
李铭昕 <715557344@qq.com>
2018-12-28 13:52:05 +0800

    Rename command proxy:create to di:init-proxy

---
+++ src/Command/InitProxyCommand.php
@@ -0,0 +1,96 @@
+<?php
+declare(strict_types=1);
+/**
+ * This file is part of Hyperf.
+ *
+ * @link     https://hyperf.org
+ * @document https://wiki.hyperf.org
+ * @contact  group@hyperf.org
+ * @license  https://github.com/hyperf-cloud/hyperf/blob/master/LICENSE
+ */
+
+namespace Hyperf\Di\Command;
+
+use Hyperf\Config\ProviderConfig;
+use Hyperf\Di\Annotation\Scanner;
+use Psr\Container\ContainerInterface;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Exception\LogicException;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Finder\Finder;
+use Symfony\Component\Finder\SplFileInfo;
+
+class InitProxyCommand extends Command
+{
+    /**
+     * @var ContainerInterface
+     */
+    private $container;
+
+    /**
+     * @var Scanner
+     */
+    private $scanner;
+
+    public function __construct(ContainerInterface $container, Scanner $scanner)
+    {
+        parent::__construct('di:init-proxy');
+        $this->container = $container;
+        $this->scanner = $scanner;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $runtime = BASE_PATH . '/runtime/container/proxy/';
+        if (is_dir($runtime)) {
+            $this->clearRuntime($runtime);
+        }
+
+        $scanDirs = $this->getScanDir();
+        $classCollection = $this->scanner->scan($scanDirs);
+
+        foreach ($classCollection as $item) {
+            try {
+                $this->container->get($item);
+            } catch (\Throwable $ex) {
+                // Entry cannot be resoleved.
+            }
+        }
+
+        $output->writeln('<info>Proxy class create success.</info>');
+    }
+
+    protected function clearRuntime($paths)
+    {
+        $finder = new Finder();
+        $finder->files()->in($paths)->name('*.php');
+
+        /** @var SplFileInfo $file */
+        foreach ($finder as $file) {
+            $path = $file->getRealPath();
+            @unlink($path);
+        }
+    }
+
+    protected function getScanDir()
+    {
+        if (!defined('BASE_PATH')) {
+            throw new LogicException('BASE_PATH is not defined.');
+        }
+
+        $file = BASE_PATH . '/config/autoload/annotations.php';
+        if (!file_exists($file)) {
+            throw new LogicException(sprintf('Annotations config path[%s] is not exists.', $file));
+        }
+
+        $annotations = include $file;
+        $scanDirs = $annotations['scan']['paths'] ?? [];
+        if (class_exists(ProviderConfig::class)) {
+            $configFromProviders = ProviderConfig::load();
+            $scanDirs = array_merge($configFromProviders['scan']['paths'] ?? [], $scanDirs);
+        }
+
+        return $scanDirs;
+    }
+}
