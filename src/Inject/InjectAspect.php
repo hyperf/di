447e4176ec43e76656a623e5621c9ca3cc8f1714
huangzhhui <huangzhwork@gmail.com>
2020-04-26 00:48:19 +0800

    Add AstVisitorCollector and Move inject logical to di component from autoload component

---
+++ src/Inject/InjectAspect.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of Hyperf.
+ *
+ * @link     https://www.hyperf.io
+ * @document https://doc.hyperf.io
+ * @contact  group@hyperf.io
+ * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
+ */
+namespace Hyperf\Di\Inject;
+
+use Hyperf\Di\Annotation\Aspect;
+use Hyperf\Di\Annotation\Inject;
+use Hyperf\Di\Aop\AbstractAspect;
+use Hyperf\Di\Aop\ProceedingJoinPoint;
+
+/**
+ * @Aspect
+ */
+class InjectAspect extends AbstractAspect
+{
+    public $annotations = [
+        Inject::class,
+    ];
+
+    public function process(ProceedingJoinPoint $proceedingJoinPoint)
+    {
+        // Do nothing, just to mark the class should be generate the proxy classs.
+        return $proceedingJoinPoint->process();
+    }
+}
